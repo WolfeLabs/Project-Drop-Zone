7f84a9e8a596f1d5c482bd0d60796cbd6366e5a6
justin <jjzwink@gmail.com>
2021-04-29 12:05:06 -0400

    first set of perks

--- Altis_Life.Altis/maverick/talent-tree-modular/modules/maverick_perkset_1/actions.cpp
+++ Altis_Life.Altis/maverick/talent-tree-modular/modules/maverick_perkset_1/actions.cpp
@@ -20,6 +20,45 @@ class VehicleLockpicked {
 	message = "Vehicle Lockpicked";
 };
 
+//Gas Delivery Experience per distance
+class GasDelivery20km {
+    expToAdd = 100;
+	message = "Fuel Delivered over 20km From Supply";
+};
+
+class GasDelivery15km {
+    expToAdd = 75;
+	message = "Fuel Delivered over 15km From Supply";
+};
+
+class GasDelivery10km {
+    expToAdd = 50;
+	message = "Fuel Delivered over 10km From Supply";
+};
+
+class GasDelivery5km {
+    expToAdd = 25;
+	message = "Fuel Delivered over 5km From Supply";
+};
+
+class GasDelivery1km {
+    expToAdd = 10;
+	message = "Fuel Delivered over 1km From Supply";
+};
+
+class GasDeliveryLess1km {
+    expToAdd = 5;
+	message = "Fuel Delivered under 1km From Supply";
+};
+
+
+
+//admin use only
+class PowerLevel {
+    expToAdd = 200;
+	message = "Experence Reward";
+};
+
 class Example {
     expToAdd = 100;
 	message = "Customizable Message";
